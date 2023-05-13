ebf54eb11a6101fa39e3a3fab4c986b067cddc15
Prabhakar Tiwari <pt17.tiwari@gmail.com>
2023-05-13 17:11:52 +0530

    fORMIK AND YUP WITH STYLED COMPONENT

--- src/Components/FORMIK_LIB.js
+++ src/Components/FORMIK_LIB.js
@@ -2,6 +2,36 @@ import { useFormik } from 'formik';
 import React, { useState } from 'react';
 import useCUSTOM from './CUSTOM_HOOKS';
 import Schema from './Schema';
+import styled,{createGlobalStyle} from 'styled-components';
+const Wrapper = styled.section`
+                display: flex;
+                align-items: center;
+                justify-content: center;
+                background-color: aqua;
+                min-height:30vh;
+                `;
+const Input = styled.input `
+              color: yellow;
+              background-color:green;
+              `
+const Button = styled.button`
+               color: ${(props)=>(props.primary ? 'red' : 'blue')};
+               font-size: 40px;
+               background-color:white;
+               &:hover{
+                background-color:yellow;
+               }
+               `
+const Secondbutton = styled(Button)`
+                background-color:#a61308;
+                &:hover{
+                  background-color:#7e98de;
+                }
+                `
+export const  Globalstyled = createGlobalStyle`
+               body{
+                 background-color:#eaebe1;
+               }`
 
 const FORMIK_LIB = () => {
   const [formData,setformData] = useState({});
@@ -22,25 +52,36 @@ const FORMIK_LIB = () => {
     })
     useCUSTOM(formData);
   return (
-    <div>
+    <>
+     <Wrapper>
       {/* <form onSubmit={formik.handleSubmit}> */}
       <form onSubmit={handleSubmit}>
         {/* <input type="text" name='name' value={formik.values.name} onChange={formik.handleChange}/> */}
-        <input type="text" name='name' value={values.name} onBlur={handleBlur} onChange={handleChange}/>
+        <label>Enter the Name </label>
+        <Input type="text" name='name' value={values.name} onBlur={handleBlur} onChange={handleChange}/>
         {/* <span style={{color: 'red'}}>{formik.errors.name}</span> */}
         {errors.name && touched.name?
         (<span style={{color: 'red'}}>{errors.name}</span>):null}
         <br />
-        <input type="text" name='email' value={values.email} onBlur={handleBlur} onChange={handleChange} />
+        <label>Enter the Email </label>
+
+        <Input type="text" name='email' value={values.email} onBlur={handleBlur} onChange={handleChange} />
         {errors.email && touched.email?
         (<span style={{color: 'red'}}>{errors.email}</span>):null}
-        <br />
-        <button type="submit">Submit</button>
+        <br /> <br />
+        <label>Submit the form </label>
+        <Button type="submit" primary>Submit</Button>
+        <Button type="submit">Submit</Button>
+        <Secondbutton type="submit">Submit</Secondbutton>
+        <Secondbutton type="submit" primary>Submit</Secondbutton>
       </form>
+      </Wrapper>
+      <Wrapper>
       <h1> this is the value of name:- {values.name}</h1>
       <h1>this is the value of email:- {values.email}</h1>
       <h1>{JSON.stringify(formData)}</h1>
-    </div>
+      </Wrapper>
+    </>
   )
 }
 
--- src/index.js
+++ src/index.js
@@ -1,21 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
-import store from './Store';
-import {Provider} from 'react-redux';
-import FORMIK_LIB from './Components/FORMIK_LIB';
-import Axios_get from './Crud Operation/Axios_get';
-import Axios_Post from './Crud Operation/Axios_Post';
-import Create_Post from './Crud Operation/Create_Post';
-import { BrowserRouter } from 'react-router-dom';
-import Crud_Store from './Crud Operation/Crud_Store';
+// import App from './App';
+// // import store from './Store';
+// import {Provider} from 'react-redux';
+import FORMIK_LIB,{Globalstyled} from './Components/FORMIK_LIB';
+// import Globalstyled from './Components/GlobalStyle';
+// import Axios_get from './Crud Operation/Axios_get';
+// import Axios_Post from './Crud Operation/Axios_Post';
+// import Create_Post from './Crud Operation/Create_Post';
+// import { BrowserRouter } from 'react-router-dom';
+// import Crud_Store from './Crud Operation/Crud_Store';
 // output will come in console
-store.subscribe(() => console.log(store.getState()));
+// store.subscribe(() => console.log(store.getState()));
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-  <FORMIK_LIB />
+  <Globalstyled />
+     <FORMIK_LIB/>
   </React.StrictMode>
 );
 
